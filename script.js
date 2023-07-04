821de501c0f626c9173a60f55696c42eda7027ed
bodV6 <fairybod@gmail.com>
2023-07-04 18:58:49 +0100

    fetch Api , paths

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-fetch("https://api.adviceslip.com/advice")
-  .then((response) => response.json())
-  .then((data) => console.log(data.slip.advice));
+// DOM Elements
+const adviceButton = document.querySelector("button");
+
+const adviceId = document.querySelector("#advice-id>span");
+
+const adviceText = document.querySelector(".advice-text");
+
+
+// Click Button event
+adviceButton.addEventListener("click", getAdvice);
+
+
+
+// Fetch Api
+async function getAdvice() {
+  const response = await fetch("https://api.adviceslip.com/advice");
+  let data = await response.json();
+  loadNewAdvice(data);
+}
+
+// Display Api
+function loadNewAdvice(advice) {
+  adviceId.innerText = advice.slip.id;
+  adviceText.innerText = advice.slip.advice;
+}
